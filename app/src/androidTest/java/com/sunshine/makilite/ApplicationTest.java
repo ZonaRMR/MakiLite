bb51c1b9e3dab589eca77919c6759bc304e69612
sfilmak <savas.savchin@gmail.com>
2017-02-21 20:49:53 +0200

    Create ApplicationTest.java

---
+++ app/src/androidTest/java/com/sunshine/makilite/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.sunshine.makilite;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by Roman on 2/21/2017.
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
