42bcc34c233c837f5c10ba633494931a325f0f47
sfilmak <savas.savchin@gmail.com>
2017-03-05 22:17:14 +0200

    Update CustomShortcutActivity.java

--- app/src/main/java/com/sunshine/makilite/activities/CustomShortcutActivity.java
+++ app/src/main/java/com/sunshine/makilite/activities/CustomShortcutActivity.java
@@ -1,3 +1,15 @@
+/**  Copyright (C) 2016-2017  Roman Savchyn/Sunshine Apps
+ Code is taken from:
+ - Folio for Facebook by creativetrendsapps. Thank you!
+ - Simple for Facebook by creativetrendsapps. Thank you!
+ - FaceSlim by indywidualny. Thank you!
+ - Toffed by JakeLane. Thank you!
+ - SlimSocial by  Leonardo Rignanese. Thank you!
+ - MaterialFBook by ZeeRooo. Thank you!
+ - Simplicity by creativetrendsapps. Thank you!
+ Copyright notice must remain here if you're using any part of this code.
+ **/
+
 package com.sunshine.makilite.activities;
 
 import android.app.Activity;
